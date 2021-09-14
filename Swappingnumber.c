29aa790f5d203a648b3f5b09e2f499f555ee63df
Adityanarayanojha <90720550+Adityanarayanojha@users.noreply.github.com>
2021-09-15 00:36:14 +0530

    Update Swappingnumber.c

--- Swappingnumber.c
+++ Swappingnumber.c
@@ -3,16 +3,16 @@ int shift(int *a ,int *b, int *c);
 void main()
 {
     int x=5,y=8,z=10;
-    printf("Before swapping ");
-    printf("x=%d",x);
-    printf("y=%d",y);
-    printf("z=%d",z);
+    printf("Before swapping /n");
+    printf("x = %d\n",x);
+    printf("y = %d\n ",y);
+    printf("z = %d",z);
     printf("\n");
     shift(&x,&y,&z);
     printf("After swapping ");
-    printf("x=%d",x);
-    printf("y=%d",y);
-    printf("z=%d",z);
+    printf("x = %d\n",x);
+    printf("y = %d\n",y);
+    printf("z = %d",z);
 }
 int shift(int*a,int*b,int*c)
 {
