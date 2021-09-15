65f99faf581c9bd6123c04e68e83aab48da7801c
Adityanarayanojha <90720550+Adityanarayanojha@users.noreply.github.com>
2021-09-15 23:54:20 +0530

    Developed by Aditya Narayan Ojha

--- Swappingnumber.c
+++ Swappingnumber.c
@@ -2,14 +2,22 @@
 int shift(int *a ,int *b, int *c);
 void main()
 {
-    int x=5,y=8,z=10;
-    printf("Before swapping /n");
+    int x,y,z;
+
+    printf("\nEnter 1st number x :");
+    scanf("%d",&x);
+    printf("\nEnter 2nd number y :");
+    scanf("%d",&y);
+    printf("\nEnter 3rd number z :");
+    scanf("%d",&z);
+
+    printf("Before swapping \n");
     printf("x = %d\n",x);
     printf("y = %d\n ",y);
     printf("z = %d",z);
     printf("\n");
     shift(&x,&y,&z);
-    printf("After swapping ");
+    printf("After swapping \n");
     printf("x = %d\n",x);
     printf("y = %d\n",y);
     printf("z = %d",z);
